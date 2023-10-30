a2fce630a8b039d57b210715884c37e86290c126
Silvi Fitria <silvifitria096@gmail.com>
2023-10-30 17:37:20 +0700

    refactor: change minus to point

--- contact.py
+++ contact.py
@@ -11,9 +11,9 @@ def load_kontak():
 # Fungsi untuk menampilkan kontak
 def display_kontak(daftar_kontak):
     if not daftar_kontak:
-        print("+-------------------+")
+        print("+...................+")
         print("Daftar kontak kosong")
-        print("+-------------------+")
+        print("+...................+")
     else:
         for kontak in daftar_kontak:
             print("+-------------------+")
