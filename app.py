322499e7183583475e8651aa68faaa482c32e847
unknown <arzaitiamiyasih@gmail.com>
2023-10-30 12:04:55 +0700

    refactor: add and remove words

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ import contact
 daftar_kontak = contact.load_kontak()
 
 while True:
-    print("###### Menu ######")
+    print("--------------------")
+    print("        Menu        ")
+    print("--------------------")
     print("1. Daftar Kontak")
     print("2. Tambah Kontak")
     print("3. Hapus Kontak")
     print("4. Cari Kontak")
     print("0. Keluar Program")
-    print("###### Menu ######")
 
     menu = input("Pilih menu : ")
 
@@ -22,11 +23,12 @@ while True:
     elif menu == "2":
         kontak = contact.new_kontak()
         daftar_kontak.append(kontak)
+        print("Kontak berhasil ditambahkan!")
     elif menu == "3":
         contact.hapus_kontak(daftar_kontak)
     elif menu == "4":
         contact.cari_kontak(daftar_kontak)
     else:
-        print("Menu tidak tersedia")
+        print("Menu tidak tersedia. Silahkan pilih 1, 2, 3, 4, atau 0")
 
 print("Program selesai berjalan, sampai jumpa")
